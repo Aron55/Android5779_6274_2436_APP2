84917fafcd1b3b77a357163fc3efadd1810d0697
יעקב אלבז <elbaz.yaacov@gmail.com>
2019-01-08 15:51:25 +0200

    envoie des drawables

---
+++ app2/src/main/java/com/arons/android5779_6274_2436_app2/Controller/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.arons.android5779_6274_2436_app2.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arons.android5779_6274_2436_app2.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
