b43366c483892afddafa4d96c2b5e85ff73c4913
Aron Scemama <aronscemama55@gmail.com>
2019-01-08 15:39:47 +0200

    Ajoute de fonction au Backend

--- app2/src/main/java/com/arons/android5779_6274_2436_app2/Model/Datasource/SQL_DBManager.java
+++ app2/src/main/java/com/arons/android5779_6274_2436_app2/Model/Datasource/SQL_DBManager.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 
 import com.arons.android5779_6274_2436_app2.Model.Backend.DBManager;
 import com.arons.android5779_6274_2436_app2.Model.Entities.Classes.Driver;
+import com.arons.android5779_6274_2436_app2.Model.Entities.Classes.Ride;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -25,8 +26,29 @@ public class SQL_DBManager implements DBManager {
 
     }
 
+
     @Override
-    public List<Driver> getListOfDriver() {
+    public List<Driver> getListOfDriver(Context context) {
+        return null;
+    }
+
+    @Override
+    public boolean addDriver(Driver driver, Context context) {
+        return false;
+    }
+
+    @Override
+    public List<Ride> getListOfFreeRide(Context context) {
+        return null;
+    }
+
+    @Override
+    public List<Ride> getListOfDoneRide(Context context) {
+        return null;
+    }
+
+    @Override
+    public List<Ride> getListOfRideOfThisDriver(Driver driver, Context context) {
         return null;
     }
 }
