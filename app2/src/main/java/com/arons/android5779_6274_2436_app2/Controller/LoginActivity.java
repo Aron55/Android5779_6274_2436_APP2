2ec09c38705b1a08276ec9aa36dfcc7e3bf5bd70
יעקב אלבז <elbaz.yaacov@gmail.com>
2019-01-10 13:06:41 +0200

    rajout de logic pour loginactivity

--- app2/src/main/java/com/arons/android5779_6274_2436_app2/Controller/LoginActivity.java
+++ app2/src/main/java/com/arons/android5779_6274_2436_app2/Controller/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.arons.android5779_6274_2436_app2.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.arons.android5779_6274_2436_app2.Model.Backend.DBManager;
 import com.arons.android5779_6274_2436_app2.Model.Backend.DBManager_Factory;
@@ -11,28 +16,46 @@ import com.arons.android5779_6274_2436_app2.R;
 
 import java.util.List;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
+
+    private EditText _email;
+    private EditText _pwd;
+    private TextView _buttonLog;
+    private TextView _buttonToSign;
     private List<Ride> rides;
+    private void findViews() {
+        _email = ((EditText) findViewById(R.id.editMail));
+        _pwd = (EditText) findViewById(R.id.editpwd);
+        _buttonLog = (TextView) findViewById(R.id.buttonLog);
+        _buttonToSign = (TextView) findViewById(R.id.buttonToSign);
+
+        _buttonLog.setOnClickListener(this);
+        _buttonToSign.setOnClickListener(this);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViews();
 
-        SQL_DBManager.notifyToRideList(new SQL_DBManager.NotifyDataChange<List<Ride>>() {
-            @Override
-            public void OnDataChanged(List<Ride> obj) {
-                // Cet fonction sera lance quand la liste des rides change
-                rides = obj;
-            }
+    }
 
-            @Override
-            public void onFailure(Exception exception) {
+    public void onClick(View v) {
 
-            }
-        });
+        if (v.getId() == R.id.buttonLog) {
+            checkLog(_email.getText().toString(),_pwd.getText().toString());
+        }
+        if(v.getId() == R.id.buttonToSign){
+            Intent logToSign = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(logToSign);
+        }
+    }
 
+    private boolean checkLog(String mail,String password){
+        return false;
     }
 
     @Override
